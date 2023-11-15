08d0b300353e53a156383960b613285c50a004ba
Veritaaaas <rodneyjamesmmaisog@iskolarngbayan.pup.edu.ph>
2023-11-15 09:50:32 +0800

    connected the javascript functions to the html

--- script.js
+++ script.js
@@ -41,53 +41,74 @@ function operate(operator, num1, num2)
 
 window.onload = function() {
 
-    buttons = document.querySelectorAll("buttons");
-    display = document.querySelector("display");
-    let num1 = "";
-    let num2 = "";
+    buttons = document.querySelectorAll(".buttons");
+    current = document.querySelector(".current");
+    previous = document.querySelector(".previous");
+    let display_current = "";
+    let num1 = 0;
+    let num2 = 0;
     let operator = "";
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function() 
         {
-            if (buttons[i].value === "clear")
+            if (buttons[i].dataset.value === "clear")
             {
                 num1 = "";
                 num2 = "";
                 operator = "";
+                display_current = "";
+                previous.innerHTML = "";
+                current.innerHTML = "";
             }
-            else if (buttons[i].value === "add")
+            else if (buttons[i].dataset.value === "+")
             {
                 operator = "+";
+                previous.innerHTML = display_current + " " + operator;
+                display_current = "";
             }
-            else if (buttons[i].value === "subtract")
+            else if (buttons[i].dataset.value === "-")
             {
                 operator = "-";
+                previous.innerHTML = display_current + " " + operator;
+                display_current = "";
             }
-            else if (buttons[i].value === "multiply")
+            else if (buttons[i].dataset.value === "*")
             {
                 operator = "*";
+                previous.innerHTML = display_current + " " + operator;
+                display_current = "";
             }
-            else if (buttons[i].value === "divide")
+            else if (buttons[i].dataset.value === "/")
             {
                 operator = "/";
+                previous.innerHTML = display_current + " " + operator;
+                display_current = "";
             }
-            else if (buttons[i].value === "equals")
+            else if (buttons[i].dataset.value === "=")
             {
                 num1 = operate(operator, num1, num2);
                 num2 = "";
                 operator = "";
+                display_current = String(num1);
+                current.innerHTML = num1;
             }
+
             else
             {
                 if (operator === "")
                 {
-                    num1 += buttons[i].value;
+                    display_current += buttons[i].dataset.value;
+                    current.innerHTML = display_current;
+                    num1 = Number(display_current);
                 }
                 else
                 {
-                    num2 += buttons[i].value;
-                }
+                    display_current += buttons[i].dataset.value
+                    current.innerHTML = display_current;
+                    previous.innerHTML = num1 + " " + operator + " " + display_current;
+                    num2 = Number(display_current);
+                }  
             }
         });
     }
